1cc834df75e4691cccb5f8250cdd1d9b60428dfa
Nick Ebbutt <nick@ebbutt.net>
2014-03-31 22:08:14 +0100

    Add to readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Chorus-demo
 ===========
 
 Some simple Chorus examples
+
+Run the JUnit suite AllChorusTests to see these in action
+
+Alternatively run the .bat file runAllChorusTests.sh on 'nix or runAllChorusTests.bat on windows
--- src/AllChorusTests.java
+++ src/AllChorusTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
  * Created by nick on 10/03/2014.
  *
  * Run all the chorus examples as a JUnit test suite
+ * Here you can pass in all the parameters you would usually pass to Chorus on the command line
  */
 @RunWith(ChorusSuite.class)
 public class AllChorusTests {
 
     public static String getChorusArgs() {
-        return "-f src -h demo -c";
+        return "-f src " +     //look for feature files under src beneath the working directory
+               "-h demo " +    //look for handler classes in packages starting with demo
+               "-c ";          //use console mode for output
+
     }
 
 }
